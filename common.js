9118c26c816df5fe00ea95f80fe2b16af16006ad
Anton Zhevak <antzhe@gmail.com>
2013-10-30 19:34:16 +0600

    contenteditable, чикуенок

--- common.js
+++ common.js
@@ -1,8 +1,3 @@
-function addContent() {
-    var el = document.getElementById('content');
-    el.innerHTML = el.innerHTML + ' Абракадабра.';
-}
-
 function spoile() {
     var el = document.getElementById('spoiler');
     el.style.display = el.style.display === 'block' ? 'none' : 'block';
